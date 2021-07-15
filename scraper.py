817b9caf774323a3ea01ac843c9ac76afe938e4c
Dylan Nguyen <dylannguyen1000@gmail.com>
2021-07-15 17:37:35 -0400

    refactor: cleaned up code

--- scraper.py
+++ scraper.py
@@ -3,25 +3,37 @@ from bs4 import BeautifulSoup
 from openpyxl import load_workbook
 import re
 
-workbook = load_workbook('info.xlsx')
-worksheet = workbook.active
+# environment variables
+LINK='https://apps.senate.virginia.gov/Senator/index.php'
+FILE_NAME='info.xlsx'
+SHEET_NAME='VA Senate'
+NAME_SELECTOR='h3'
 
-with open('urls.txt') as f:
+# get page
+page = requests.get(LINK)
+content = page.text
+
+# scraper init
+scraper  = BeautifulSoup(page.content, 'html.parser')
+body = scraper.body
+
+# workbook initialization
+workbook = load_workbook(FILE_NAME)
+worksheet = workbook.create_sheet(SHEET_NAME)
+
+for link in scraper.find_all('a'):
     row_num = 1
-    for line in f:
-        page = requests.get(line.strip('\n'))
-        content = page.text
-        scraper  = BeautifulSoup(page.content, 'html.parser')
-        body = scraper.body
+    link = "https:" + link
+    page = requests.get(link)
 
-        name = scraper.find('h3').text
-        # phone_numbers = list(re.findall(r"((?:\d{3}|\(\d{3}\))?(?:\s|-|\.)?\d{3}(?:\s|-|\.)\d{4})", content))
-        phone_numbers = list(re.findall(r"((?:\d{3}|\(\d{3}\)){1}(?:\s|-|\.)?\d{3}(?:\s|-|\.)\d{4})", content))
-        emails = list(re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}", content))
+    name = scraper.find(NAME_SELECTOR).text
+    # phone_numbers = list(re.findall(r"((?:\d{3}|\(\d{3}\))?(?:\s|-|\.)?\d{3}(?:\s|-|\.)\d{4})", content))
+    phone_numbers = list(re.findall(r"((?:\d{3}|\(\d{3}\)){1}(?:\s|-|\.)?\d{3}(?:\s|-|\.)\d{4})", content))
+    emails = list(re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}", content))
 
-        worksheet.cell(row=row_num, column=1, value=name)
-        worksheet.cell(row=row_num, column=2, value=phone_numbers[0])
-        worksheet.cell(row=row_num, column=3, value=emails[0])
-        row_num+=1
+    worksheet.cell(row=row_num, column=1, value=name)
+    worksheet.cell(row=row_num, column=2, value=phone_numbers[0])
+    worksheet.cell(row=row_num, column=3, value=emails[0])
+    row_num+=1
 
-workbook.save('info.xlsx')
+workbook.save(FILE_NAME)

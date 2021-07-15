2de57a8ac436a0bc747b307773906aee72956cd5
Dylan Nguyen <dylannguyen1000@gmail.com>
2021-07-15 13:40:08 -0400

    feat: obtained urls of each representative's page

---
+++ url.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+# senate
+# page = requests.get("https://msa.maryland.gov/msa/mdmanual/05sen/html/sendist.html")
+
+# house
+page = requests.get("https://msa.maryland.gov/msa/mdmanual/06hse/html/hsedist.html")
+
+
+scraper  = BeautifulSoup(page.content, 'html.parser')
+body = scraper.body
+
+with open('urls.txt', 'a') as f:
+    for link in scraper.find_all('a'):
+        f.write("https://msa.maryland.gov")
+        f.write(link.get('href'))
+        f.write("\n")

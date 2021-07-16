95155a2e2d2c6d3d0338fbbd8785bfd9990389a7
Dylan Nguyen <dylannguyen1000@gmail.com>
2021-07-16 10:37:29 -0400

    feat: added debugging

--- scraper.py
+++ scraper.py
@@ -8,10 +8,14 @@ LINK='https://apps.senate.virginia.gov/Senator/index.php'
 FILE_NAME='info.xlsx'
 SHEET_NAME='VA Senate'
 NAME_SELECTOR='h3'
+DEBUG=False
 
 # get page
 page = requests.get(LINK)
 content = page.text
+
+if DEBUG:
+    print(content)
 
 # scraper init
 scraper  = BeautifulSoup(page.content, 'html.parser')
@@ -27,9 +31,13 @@ for link in scraper.find_all('a'):
     page = requests.get(link)
 
     name = scraper.find(NAME_SELECTOR).text
-    # phone_numbers = list(re.findall(r"((?:\d{3}|\(\d{3}\))?(?:\s|-|\.)?\d{3}(?:\s|-|\.)\d{4})", content))
     phone_numbers = list(re.findall(r"((?:\d{3}|\(\d{3}\)){1}(?:\s|-|\.)?\d{3}(?:\s|-|\.)\d{4})", content))
     emails = list(re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}", content))
+
+    if DEBUG:
+        print(name)
+        print(phone_numbers)
+        print(emails)
 
     worksheet.cell(row=row_num, column=1, value=name)
     worksheet.cell(row=row_num, column=2, value=phone_numbers[0])
